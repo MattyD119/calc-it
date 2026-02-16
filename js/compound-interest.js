c0fc6d96a178d72c0e3d5eac49b65e5d7d1a759f
Matthew Day <MattyD119@users.noreply.github.com>
2026-02-16 08:37:11 +0000

    Added compound js

--- js/compound-interest.js
+++ js/compound-interest.js
@@ -8,6 +8,8 @@ const calculateButton = document.getElementById("calculate-button");
 
 calculateButton.addEventListener("click", calculateCompoundInterest);
 
+const tableBody = document.getElementById("table-body");
+
 function calculateCompoundInterest() {
   const principle = parseFloat(initialInvestment.value);
   const rate = parseFloat(interestRate.value);
@@ -15,4 +17,36 @@ function calculateCompoundInterest() {
   const years = parseFloat(yearsInvested.value);
   const amount = parseFloat(depositAmount.value);
   const depfreq = parseFloat(depositFrequency.value);
+
+  tableBody.innerHTML = "";
+
+  let currentBalance = principle;
+  let runningInterest = 0;
+
+  for (let year = 1; year <= years; year++) {
+    const row = document.createElement("tr");
+
+    const yearCell = document.createElement("td");
+    const interestCell = document.createElement("td");
+    const accruedCell = document.createElement("td");
+    const balanceCell = document.createElement("td");
+
+    const newBalance =
+      currentBalance * Math.pow(1 + rate / 100 / compFreq, compFreq * 1);
+    let yearsInterest = newBalance - currentBalance;
+    currentBalance = newBalance;
+    runningInterest += yearsInterest;
+
+    yearCell.textContent = year;
+    interestCell.textContent = "£" + yearsInterest.toFixed(2);
+    accruedCell.textContent = "£" + runningInterest.toFixed(2);
+    balanceCell.textContent = "£" + currentBalance.toFixed(2);
+
+    row.appendChild(yearCell);
+    row.appendChild(interestCell);
+    row.appendChild(accruedCell);
+    row.appendChild(balanceCell);
+
+    tableBody.appendChild(row);
+  }
 }
